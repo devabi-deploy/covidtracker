f9c64ffd3be6caad350271fdab85cd10813c134b
devabi-deploy <devabi661528@gmail.com>
2022-06-28 16:01:51 +0700

    Build chart covid

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,6 +1,33 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import { fetchDailyData } from "../../components/api"
+import { Line, Bar } from "react-chartjs-2"
+
+import styles from "./Charts.module.css"
 
 const Charts = () => {
+    const [dailyData, setDailyData] = useEffect({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        console.log(dailyData);
+
+        fetchAPI();
+    }, [])
+
+    const lineChart = (
+        dailyData[0] 
+        ? (
+        <Line
+            data={{
+                labels: '',
+                datasets: [{}, {}],
+            }}
+        />) : null
+    );
+
     return (
         <h1>
             Charts
